f5e09f035e853d5fd6c85955cb88c4a5794b9b53
Mishu78 <meherun2002@gmail.com>
2026-02-16 16:16:39 +0600

    by taking variable onclick eventhandler added

---
+++ events/events.js
@@ -0,0 +1,7 @@
+ //option2 for button click
+ function makeYellow(){
+            document.body.style.backgroundColor='yellow';
+        }
+        function makeRed(){
+            document.body.style.backgroundColor='red';
+        }
