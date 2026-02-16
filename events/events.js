28af5aa8ebc03986ab8dfa1d5f3d19039adac6ba
Mishu78 <meherun2002@gmail.com>
2026-02-16 16:38:50 +0600

    onclick through addeventlistener

--- events/events.js
+++ events/events.js
@@ -1,7 +1,19 @@
- //option2 for button click
+ //option2 for button click option 2
  function makeYellow(){
             document.body.style.backgroundColor='yellow';
         }
         function makeRed(){
             document.body.style.backgroundColor='red';
         }
+    //onclick by id option 3
+    const btnMakeBlue= document.getElementById('btn-make-blue');
+       //console.log(btnMakeBlue);
+       btnMakeBlue.onclick = function makeBlue(){
+        document.body.style.backgroundColor='blue';
+       }
+//option 4
+    const btnMakePurple=document.getElementById('btn-make-purple');
+       btnMakePurple.onclick=makePurple;
+       function makePurple(){
+        document.body.style.backgroundColor="purple";
+       }
