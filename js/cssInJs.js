c8c7f92b0f1e6e9da505c95d03a33a3128a70a31
Mishu78 <meherun2002@gmail.com>
2026-02-16 10:21:55 +0600

    inline clss and dynamic css class added

--- js/cssInJs.js
+++ js/cssInJs.js
@@ -1,12 +1,18 @@
 //const sections=document.getElementsByTagName('section');
 const sections=document.querySelectorAll('section');
 //console.log(sections);
-for(const section of sections){
-    console.log(section);
-    section.style.backgroundColor='lightBlue';
-    section.style.border='3px solid green';
-    section.style.marginBottom='20px';
-    section.style.borderRadius='15px';
-    section.style.padding='10px';
-}
+//inline-css class
+// for(const section of sections){
+//     console.log(section);
+//     section.style.backgroundColor='lightBlue';
+//     section.style.border='3px solid green';
+//     section.style.marginBottom='20px';
+//     section.style.borderRadius='15px';
+//     section.style.padding='10px';
+// }
+
+//dynamic class
+// for(const section of sections){
+//     section.classList.add('section-card');
+// }
 
